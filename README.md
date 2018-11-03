19f4566cabd5a347e6218ae7140bab3e54e3852a
Emilio Pomares <remilia@Emilios-iMac.local>
2018-11-03 11:59:29 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # nodejsmasterclass-assignment1
 Homework assignment 1 for Node.js masterclass from Pirple
+By Emilio Pomares
+
+A GET request to the route /hello will return a nice JSON object
+Any other requests will return a 400 (bad request) code
+
---
+++ index.js
@@ -0,0 +1,60 @@
+'use strict';
+
+const http = require('http');
+const https = require('https');
+const url = require('url');
+const fs = require('fs');
+
+var credentials =
+{
+	key: fs.readFileSync('./ssl/key.pem'),
+	cert: fs.readFileSync('./ssl/cert.pem')
+}
+
+const httpPort = 8080;
+const httpsPort = 8081;
+
+function serverFunction(req, res) {
+
+	const method = req.method.toLowerCase();
+	const parsedURL = url.parse(req.url, true);
+	const trimmedPath = parsedURL.pathname.replace(/^\/+|\/+$/g, '');
+
+	var handler = chooseHandler(method, trimmedPath);
+	handler(function(code, data) {
+		res.setHeader('Content-Type', 'application/json');
+		res.writeHead(code);
+		res.end(data);
+	});	
+
+}
+
+var httpsServer = https.createServer(credentials, serverFunction);
+httpsServer.listen(httpsPort, function() {
+	console.log("HTTPS REST API running on port " + httpsPort);
+});
+
+var httpServer = http.createServer(serverFunction);
+httpServer.listen(httpPort, function() {
+	console.log("HTTP REST API running on port " + httpPort);
+});
+
+var handlers = {};
+handlers.get = {};
+
+handlers.unhandled = function(cb) {
+	cb(400, "{'result':'bad request'}");
+}
+
+handlers.get.hello = function(cb) {
+	cb(200, "{'PI':3.1416}");
+}
+
+function chooseHandler(method, route) {
+        if( (typeof(handlers[method]) !== 'undefined') && 
+	(typeof(handlers[method][route]) !== 'undefined')) 
+	{
+			return handlers[method][route];
+        }
+        else return handlers.unhandled; 
+}
