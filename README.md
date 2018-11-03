179e54546f89dac0c4abf8b99b02a00de2415a40
Emilio Pomares <remilia@Emilios-iMac.local>
2018-11-03 12:09:01 +0100

    A few cosmetic improvements

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # nodejsmasterclass-assignment1
 Homework assignment 1 for Node.js masterclass from Pirple
-By Emilio Pomares
+By Emilio Pomares (emilio.pomares.porras@gmail.com)
 
-A GET request to the route /hello will return a nice JSON object
+A GET request to the route /hello will be rewarded with a 
+	very satisfying JSON object
 Any other requests will return a 400 (bad request) code
+
+Note that there is not any code to process
+payloads as these will be ignored
 
--- index.js
+++ index.js
@@ -9,46 +9,21 @@ var credentials =
 {
 	key: fs.readFileSync('./ssl/key.pem'),
 	cert: fs.readFileSync('./ssl/cert.pem')
-}
+};
 
 const httpPort = 8080;
 const httpsPort = 8081;
-
-function serverFunction(req, res) {
-
-	const method = req.method.toLowerCase();
-	const parsedURL = url.parse(req.url, true);
-	const trimmedPath = parsedURL.pathname.replace(/^\/+|\/+$/g, '');
-
-	var handler = chooseHandler(method, trimmedPath);
-	handler(function(code, data) {
-		res.setHeader('Content-Type', 'application/json');
-		res.writeHead(code);
-		res.end(data);
-	});	
-
-}
-
-var httpsServer = https.createServer(credentials, serverFunction);
-httpsServer.listen(httpsPort, function() {
-	console.log("HTTPS REST API running on port " + httpsPort);
-});
-
-var httpServer = http.createServer(serverFunction);
-httpServer.listen(httpPort, function() {
-	console.log("HTTP REST API running on port " + httpPort);
-});
 
 var handlers = {};
 handlers.get = {};
 
 handlers.unhandled = function(cb) {
 	cb(400, "{'result':'bad request'}");
-}
+};
 
 handlers.get.hello = function(cb) {
 	cb(200, "{'PI':3.1416}");
-}
+};
 
 function chooseHandler(method, route) {
         if( (typeof(handlers[method]) !== 'undefined') && 
@@ -58,3 +33,28 @@ function chooseHandler(method, route) {
         }
         else return handlers.unhandled; 
 }
+
+function serverFunction(req, res) {
+
+        const method = req.method.toLowerCase();
+        const parsedURL = url.parse(req.url, true);
+        const trimmedPath = parsedURL.pathname.replace(/^\/+|\/+$/g, '');
+
+        const handler = chooseHandler(method, trimmedPath);
+        handler(function(code, data) {
+                res.setHeader('Content-Type', 'application/json');
+                res.writeHead(code);
+                res.end(data);
+        });
+
+}
+
+var httpsServer = https.createServer(credentials, serverFunction);
+httpsServer.listen(httpsPort, function() {
+        console.log("HTTPS REST API running on port " + httpsPort);
+});
+
+var httpServer = http.createServer(serverFunction);
+httpServer.listen(httpPort, function() {
+        console.log("HTTP REST API running on port " + httpPort);
+});
